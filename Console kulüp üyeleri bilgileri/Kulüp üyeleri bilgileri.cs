7ea3f0485cfba1f5620464328765621477fc3f76
Merve Safa özcan <mervesafa003@gmail.com>
2025-02-07 16:12:53 +0300

    Add project files.

---
+++ Console kulüp üyeleri bilgileri/Kulüp üyeleri bilgileri.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Console_kulüp_üyeleri_bilgileri
+{
+    internal class Kulüp_üyeleri_bilgileri
+    {
+        string ad;
+        string soyad;
+        string bolum;
+        string sinif;
+        char cinsiyet;
+
+        public string AD
+        {
+            get { return ad; }
+            set { ad = value.ToUpper(); }
+        }
+
+        public string SOYAD
+        {
+            get { return soyad; }
+            set { soyad = value.ToUpper(); }
+        }
+
+        public string BOLUM
+        {
+            get { return bolum; }
+            set { bolum = value.ToUpper(); }
+        }
+
+        public string SINIF
+        {
+            get { return sinif; }
+            set { sinif = value.ToUpper(); }
+        }
+
+        public char CINSIYET
+        {
+            get { return cinsiyet; }
+            set { cinsiyet = value; }
+        }
+
+        public Kulüp_üyeleri_bilgileri()
+        {
+            ad = " ";
+           soyad = " ";
+           bolum = " ";
+           sinif = " ";
+           cinsiyet = ' ';
+        }
+    }
+}
---
+++ Console kulüp üyeleri bilgileri/Program.cs
@@ -0,0 +1,21 @@
+﻿using System.Security.Cryptography.X509Certificates;
+
+namespace Console_kulüp_üyeleri_bilgileri
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Kulüp_üyeleri_bilgileri uye = new Kulüp_üyeleri_bilgileri();
+            uye.AD = "Ada";
+            uye.SOYAD = "Sevinç";
+            uye.BOLUM = "Bilgisayar Mühendisliği";
+            uye.SINIF = "2";
+            uye.CINSIYET = 'K';
+            Console.WriteLine("Adı: " + uye.AD);
+            Console.WriteLine("Soyadı: " + uye.SOYAD);
+            Console.WriteLine("Bölümü: " + uye.BOLUM);
+            Console.WriteLine("Sınıfı: " + uye.SINIF);
+        }
+    }
+}
